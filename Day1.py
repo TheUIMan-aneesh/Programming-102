a79af33fb9b6e11275dcbb6a48bdcfc0ad785188
TheUIMan-aneesh <75133437+TheUIMan-aneesh@users.noreply.github.com>
2022-05-25 14:33:40 -0400

    Update Day1.py

--- Day1.py
+++ Day1.py
@@ -6,3 +6,15 @@ print("Akshaya Patra")
 #Create a variable 
 FavoriteNumber = 7 
 print(FavoriteNumber)
+
+
+#Types of data 
+myinteger = 5
+myfloat = 1.222
+mystring = "This is a string."
+myboolean = True
+
+print(type(myinteger))
+print(type(myfloat))
+print(type(mystring))
+print(type(myboolean))
